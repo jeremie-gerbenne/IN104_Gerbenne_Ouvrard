3881412c56f78cd2961283a964b22c23f5e97f39
TitouanEnsta <81634177+TitouanEnsta@users.noreply.github.com>
2021-05-09 19:33:38 +0200

    Update engine.py
    Correction due aux classes, petites erreurs (len, get ...)

--- projet/simulator/physics/engine.py
+++ projet/simulator/physics/engine.py
@@ -19,7 +19,7 @@ class IEngine:
     def derivatives(self, t0, y0):
         """ This is the method that will be fed to the solver
             it does not use it's first argument t0,
-            its second argument y0 is a vector containing the positions 
+            its second argument y0 is a vector containing the positions
             and velocities of the bodies, it is laid out as follow
                 [x1, y1, x2, y2, ..., xn, yn, vx1, vy1, vx2, vy2, ..., vxn, vyn]
             where xi, yi are the positions and vxi, vyi are the velocities.
@@ -28,51 +28,55 @@ class IEngine:
                 [vx1, vy1, vx2, vy2, ..., vxn, vyn, ax1, ay1, ax2, ay2, ..., axn, ayn]
             where vxi, vyi are the velocities and axi, ayi are the accelerations.
         """
+
         n = len(y0)/4
+        n = int(n)
         for i in range(n):
             y0[2*i]=y0[2*i+2*n]
             y0[2*i+1]=y0[2*i+1+2*n]
             force = Vector2(0,0)
-            corps_i = self.world[i]
-            pos_i = corps_i.pos
+            corps_i = self.world.get(i)
+            pos_i = corps_i.position
             mass_i = corps_i.mass
             for j in range(n):
                 if i!=j:
-                    corps_j = self.world[j]
-                    pos_j = corps_j.pos
+                    corps_j = self.world.get(j)
+                    pos_j = corps_j.position
                     mass_j = corps_j.mass
                     Fij = gravitational_force(pos_i,mass_i,pos_j,mass_j)
                     force = force + Fij
             y0[2*i+2*n] = Vector2.get_x(force)
             y0[2*i+1+2*n] = Vector2.get_y(force)
         return y0
-        
-        raise NotImplementedError
+
+
 
     def make_solver_state(self):
         """ Returns the state given to the solver, it is the vector y in
                 y' = f(t, y)
-            In our case, it is the vector containing the 
+            In our case, it is the vector containing the
             positions and speeds of all our bodies:
                 [x1, y1, x2, y2, ..., xn, yn, vx1, vy1, vx2, vy2, ..., vxn, vyn]
             where xi, yi are the positions and vxi, vyi are the velocities.
         """
         n = len(self.world)
         y = []
-        for i in range(n) : 
+        for i in range(n) :
             corps_i = self.world.get(i)
             pos_i = corps_i.position
             y.append(Vector2.get_x(pos_i))
             y.append(Vector2.get_y(pos_i))
-        
+
         for i in range(n) :
             corps_i = self.world.get(i)
             vel_i = corps_i.velocity
             y.append(Vector2.get_x(vel_i))
             y.append(Vector2.get_y(vel_i))
-        
-        raise NotImplementedError
+        return(y)
+
+
 
 
 class DummyEngine(IEngine):
     pass
+
