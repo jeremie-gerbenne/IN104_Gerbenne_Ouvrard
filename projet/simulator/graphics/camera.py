910bfacd6b8e3c887eb56c0176376350f5d9cb1b
jeremie-gerbenne <81633769+jeremie-gerbenne@users.noreply.github.com>
2021-05-09 19:08:36 +0200

    Update camera.py
    upload to_screen_coords from_screen_coords

--- projet/simulator/graphics/camera.py
+++ projet/simulator/graphics/camera.py
@@ -9,8 +9,26 @@ class Camera:
 
     def to_screen_coords(self, position):
         """ Converts the world-coordinate position to a screen-coordinate. """
-        raise NotImplementedError
+        x_s_max = self.screen_size[0]
+        y_s_max = self.screen_size[1]
+        scale = self.scale
+        x_w = position.get_x()
+        y_w = position.get_y()
+        x_c = self.position.get_x()
+        y_c = self.position.get_y()
+        x_s = x_s_max/2+(x_w-x_c)*scale
+        y_s = y_s_max/2+(y_w-y_c)*scale
+        return Vector2(x_s,y_s)
 
     def from_screen_coords(self, position):
         """ Converts the screen-coordinate position to a world-coordinate. """
-        raise NotImplementedError
+        x_s = position.get_x()
+        y_s = position.get_y()
+        x_s_max = self.screen_size[0]
+        y_s_max = self.screen_size[1]
+        scale = self.scale
+        x_c = self.position.get_x()
+        y_c = self.position.get_y()
+        x_w = (x_s-x_s_max/2)/scale + x_c
+        y_w = (y_s-y_s_max/2)/scale + y_c
+        return Vector2(x_w, y_w)
